5af2ddede734b6e3d3436a01e035faada5413cec
BubbleStone50 <119154247+BaorzK@users.noreply.github.com>
2024-03-05 08:17:25 +0700

    Add files via upload

--- src/BubbleS/NPCConnect/events/NPCCreatedEvent.php
+++ src/BubbleS/NPCConnect/events/NPCCreatedEvent.php
@@ -1,1 +1,32 @@
+<?php
 
+/*
+* Absolutely do not change the author to share
+* Using this library helps you create NPCs to communicate with players, create quests, etc...
+
+* Copyright © BubbleStone50 | 2024 - 2025
+* Github: BubbleStone50
+* Facebook: Trần Huy Bảo
+
+* This file is used to test and run event
+*/
+
+declare(strict_types=1);
+
+namespace BubbleS\NPCConnect\events;
+
+use pocketmine\event\entity\EntityEvent;
+use BubbleS\NPCConnect\npc\NPC;
+
+class NPCCreatedEvent extends EntityEvent{
+  
+  public NPC $npc;
+  
+  public function __construct(NPC $npc){
+    $this->npc = $npc;
+  }
+  
+  public function getNpc():NPC{
+    return $this->npc;
+  }
+}
